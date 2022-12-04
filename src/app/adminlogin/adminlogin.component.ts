4af355b2414f1b6040b030f50d5c1290516e7046
HarshaSalih <harshasalih2000@gmail.com>
2022-12-04 12:28:02 +0530

    adminlogin  commits

---
+++ src/app/adminlogin/adminlogin.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-adminlogin',
+  templateUrl: './adminlogin.component.html',
+  styleUrls: ['./adminlogin.component.css']
+})
+export class AdminloginComponent {
+
+  // if (this.username=="admin" && this.password=="nest") {
+  //   alert("valid login")
+    
+  // } else {
+  //   alert("invalid credential")
+    
+  // }
+
+}
