63a2d80a4939e7df13b8eaac4c7a48f5c1b41381
HarshaSalih <harshasalih2000@gmail.com>
2022-12-04 12:50:31 +0530

    viewcourse  commits

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { ViewcoursesComponent } from './viewcourses/viewcourses.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminloginComponent
+    AdminloginComponent,
+    ViewcoursesComponent
   ],
   imports: [
     BrowserModule,
